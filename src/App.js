2e43f76c749660fa76c94e1a431fc9fd2bd447f8
Raj141020 <rajnagwanshi1410@gmail.com>
2023-05-03 08:54:26 +0530

    Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import logo from './logo.svg';
-// import { useState } from 'react';
-import './App.css';
-// import { Users } from './Users';
-// import{Student} from './Student'
 
-import React,{useState} from 'react';
+import './App.css';
+
 
 function App() {
 
-  const [data, setData] = useState(null)
-  const [print,getPrint] = useState(false)
-
-  function getResult(val){
-    console.log(val.target.value)
-    setData(val.target.value)
-    getPrint(false)
-  }
+  
+  
   return (
     <div className="App">
-      {
-        print?<h1>{data}</h1>:null
-      }
-     <input type="text" onChange={getResult} />
-     <button onClick={()=>getPrint(true)}>Click</button>
+  
      
     </div>
   );
