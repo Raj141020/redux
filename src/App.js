1a7b571a58413f7ed7bc492cc788cf8a7261926d
Raj141020 <rajnagwanshi1410@gmail.com>
2023-05-03 12:45:02 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
 
+import React from 'react';
 import './App.css';
-
-
+import HomeContainer from './containers/HomeContainer'
+import HeaderContainer from './containers/HeaderContainer'
 function App() {
-
-  
-  
   return (
     <div className="App">
-  
-     
+      <HeaderContainer />
+
+      <HomeContainer />
     </div>
   );
 }
